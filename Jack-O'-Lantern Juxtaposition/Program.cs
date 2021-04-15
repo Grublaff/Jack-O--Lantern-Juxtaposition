ceaabb69277595631acdfe8cb0752f21039ec537
Grubla <filip.aburg@gmail.com>
2021-04-16 00:17:21 +0200

    Add project files.

---
+++ Jack-O'-Lantern Juxtaposition/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Jack_O__Lantern_Juxtaposition{
+    class Program{
+        static void Main(string[] args){
+            string[] input = Console.ReadLine().Split(" ");
+            int[] inputInt = Array.ConvertAll(input, int.Parse);
+            int total = 1;
+            foreach (int element in inputInt){
+                total *= element;
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
